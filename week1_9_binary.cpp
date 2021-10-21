e649bb53c056400f43b1dc3e7c380b62022e3fdf
realmi8 <samalegen@gmail.com>
2021-10-21 14:29:47 +0300

    may be correct version

--- week1_9_binary.cpp
+++ week1_9_binary.cpp
@@ -4,30 +4,21 @@
 using namespace std;
 
 int main() {
-    vector<int> s, w;
+    vector<int> s = {}, w = {};
     int x;
     cin >> x;
     s.push_back(x);
     w.push_back(x % 2);
     int n = x;
-    while(n > 0) {
-//        cout << n << endl << n % 2 << endl;
+
+    while (n != 0) {
         n /= 2;
-        s.push_back(n);
         w.push_back(n % 2);
     }
-    for (auto i = w.size(); i > 0; --i) {
+    for (int i = (w.size() - 2); i >= 0; --i) {
         cout << w[i];
     }
     cout << endl;
-    for (auto i = 0; i < ((w.size()) - 1); ++i) {
-        cout << w[i];
-    }
 
-    cout << endl;
-    for (auto i = w.size(); i > 0; --i) {
-        cout << w[i];
-    }
-    cout << endl;
     return 0;
 }
