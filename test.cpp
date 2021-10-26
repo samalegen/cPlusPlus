7629bfa0e498b3e5abb450476bf98a8823cb6aad
x550ln <samalegen@gmail.com>
2021-10-26 13:30:50 +0300

    solution 10th week 2

--- test.cpp
+++ test.cpp
@@ -1,47 +1,59 @@
 #include <iostream>
 #include <vector>
-#include <string>
 
+struct Day {
+    int num;
+    std::string task;
+};
 
+void add(std::vector<std::vector<std::string>>& s) {
+    int i;
+    std::cin >> i;
+    std::cin >> s[i - 1].push_back();
+}
 
-int main() {
-    int Q;
-    std::string question;
-    int count;
-    std::vector<bool> v;
-    std::vector<int> w;
-    std::cin >> Q;
+void dump(const int& i)  {
 
-    for (int i = 0; i < Q; i++) {
-        std::cin >> question;
-        if (question == "WORRY_COUNT") {
-            int worry = 0;
-            if (v.size() == 0) {
-                worry = 0;
-            }
-            else {
-                for (int i = 0; i < v.size(); ++i) {
-                    if (v[i] == true) {
-                        ++worry;
-                    }
-                }
-            }
-            w.push_back(worry);
-        }
-        else {
-            std::cin >> count;
-            if (question == "COME") {
-                v.resize(v.size() + count, false);
-            }
-            if (question == "WORRY") {
-                v[count] = true;
-            }
-            if (question == "QUIET") {
-                v[count] = false;
+}
+
+void next(int& month_count, std::vector<std::vector<std::string>>& s, const std::vector<int>& year) {
+    month_count++;
+    std::vector<std::vector<std::string>> tmp = s;
+    //s.clear();
+    s.resize(year[month_count]);
+    if (s.size() < tmp.size()) {
+        for (int i = s.size(); i <= tmp.size(); ++i) {
+            if (tmp[i].size() != 0) {
+                s[s.size() - 1].push_back(tmp[i]);
             }
         }
     }
-    for (auto i : w) {
-        std::cout << i << "\n";
+
+}
+int main() {
+    std::vector<int> year = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31,};
+    std::vector<std::vector<std::string>> month;
+    int month_count = 0;
+    month.resize(year[month_count]);
+    std::string operation;
+    int count;
+    std::cin >> count;
+    for (int i = 0; i < count; ++i) {
+        std::cin >> operation;
+        if (operation == "ADD") {
+            add(month);
+        }
+        if (operation == "NEXT") {
+            next(month_count, month, year);
+        }
+
+    }
+    int b = 1;
+    for (auto i : month) {
+        for (auto k : i) {
+            std::cout << b << ": " << k << " ";
+            ++b;
+        }
+        
     }
 }
