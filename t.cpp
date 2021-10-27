26a914c59e376ffebdc19ae4b975188e6cd67beb
realmi8 <samalegen@gmail.com>
2021-10-27 11:38:19 +0300

    new

---
+++ t.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+std::vector<char> toCharVector(const std::string& x) {
+    std::vector<char> result;
+    for (const auto& i : x) {
+        result.pish_back(i);
+        return result;
+}
+
+
+bool buildCharCounters(const std::string& a, const std::string b) {
+    std::vector<char> vA = toCharVector(a);
+    std::vector<char> vB = toCharVector(b);
+    std::map<char, int> m:
+        for (const auto& i : m) {
+
+        }
+    
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+
+}
